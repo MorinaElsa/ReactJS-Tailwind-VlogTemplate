046f83b59e828dbe21ff84b086b7c59a32ef6fb5
MorinaElsa <morinaelsa01@gmail.com>
2024-04-23 16:09:46 +0200

    Links for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
     <div className="flex justify-between items-center h24 px-4 max-w-[1240px] mx-auto">
       <h2 className="text-3xl font-cabin hover:text-hover-green">Meranda</h2>
       <ul className="flex font-mono">
-        <li className="p-4">HOME</li>
-        <li className="p-4">CATEGORIES</li>
-        <li className="p-4">HEALTH</li>
-        <li className="p-4">DESIGN</li>
+        <li className="p-4"> <a href="#"> HOME </a></li>
+        <li className="p-4"><a href="#">CATEGORIES</a></li>
+        <li className="p-4"><a href="#">HEALTH</a></li>
+        <li className="p-4"><a href="#">DESIGN</a></li>
       </ul>
     </div>
   );
