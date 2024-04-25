842e787510d6d4ef92f443fbb0a9d9d4f60a2fc3
MorinaElsa <morinaelsa01@gmail.com>
2024-04-25 10:40:23 +0200

    responsive Influencers section

--- src/components/Influencers.js
+++ src/components/Influencers.js
@@ -5,7 +5,7 @@ function Influencers() {
   return (
     <div className="w-full bg-influencer-background py-12 font-poppins">
       <div className=" max-w-screen-2xl  mx-auto ">
-        <div className="text-center mb-12">
+        <div className="text-center mb-12 m-auto lg:max-w-5xl sm:sm:max-w-md">
           <span className="text-sm text-gray-400 font-medium ">TESTIMONY</span>
           <h2 className="text-3xl text-gray-500 mb-10">Our Influencers </h2>
           <p className="text-sm max-w-screen-sm m-auto text-gray-500 mb-10 leading-6">
@@ -16,18 +16,17 @@ function Influencers() {
             that was left from its origin.
           </p>
         </div>
-
-        <div className="m-4 grid grid-cols-4 gap-4  text-gray-500 text-sm ">
+        <div className="m-4 grid  gap-4  text-gray-500 text-sm lg:grid-cols-2 sm:grid-cols-1 2xl:grid-cols-4">
           {dataI.map((influencer, index) => (
             <div className="min-h-[250px] " key={influencer.id}>
-              <div className="flex justify-center items-center  mb-8">
+              <div className="flex justify-center items-center mb-8">
                 <img
-                  className="rounded-full w-1/2 h-1/2"
+                  className="rounded-full 2xl:w-1/2 2xl:h-1/2 sm:w-1/4 sm:h-1/4 "
                   src={influencer.img}
                   alt=" Influencers"
                 />
               </div>
-              <div className="px-12 text-center text-xs leading-5">
+              <div className="px-12 text-center text-xs leading-5 2xl:max-w-lg lg:max-w-sm sm:max-w-xs m-auto">
                 <p className="mb-6">{influencer.quote}</p>
                 <p className="text-lg text-gray-900">{influencer.fullname}</p>
                 <span>{influencer.place}</span>
--- src/data/influencersData.js
+++ src/data/influencersData.js
@@ -3,7 +3,7 @@ const dataI = [
     Id: "1",
     img: require("../img/influencer.png"),
     quote:
-      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
     fullname: " John Doe ",
     place: "Kosovo",
   },
@@ -11,7 +11,7 @@ const dataI = [
     Id: "2",
     img: require("../img/influencer.png"),
     quote:
-      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.  ",
     fullname: " John Doe ",
     place: "Moroko",
   },
@@ -19,7 +19,7 @@ const dataI = [
     Id: "3",
     img: require("../img/influencer.png"),
     quote:
-      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
     fullname: " John Doe ",
     place: "Albania",
   },
@@ -27,7 +27,7 @@ const dataI = [
     Id: "4",
     img: require("../img/influencer.png"),
     quote:
-      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
     fullname: " John Doe ",
     place: "Malta",
   },
