4b74246af8af0c9015976073532ea373864d4b33
MorinaElsa <morinaelsa01@gmail.com>
2024-05-02 10:33:41 +0200

    responsive navbar minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     <nav className="fixed top-0 z-50 w-full bg-white shadow-lg">
       <div className="flex justify-between items-center h-16 px-4 max-w-[1240px] mx-auto">
         <h2 className="text-3xl font-cabin hover:text-hover-green">Meranda</h2>
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button
             onClick={toggleMenu}
             className="text-xl text-gray-800 focus:outline-none"
@@ -21,9 +21,9 @@ const NavBar = () => {
           </button>
         </div>
         <ul
-          className={`uppercase md:flex ${
+          className={`uppercase lg:flex ${
             isMenuOpen ? "block" : "hidden"
-          } font-mono md:mt-0 mt-4 absolute md:relative top-full left-0 md:top-0 md:left-auto md:flex flex-col md:flex-row bg-white sm:bg-transparent w-full sm:w-auto justify-center items-center`}
+          } font-mono lg:mt-0 mt-4 absolute lg:relative top-full left-0 lg:top-0 md:left-auto lg:flex flex-col lg:flex-row bg-white lg:bg-transparent w-full lg:w-auto justify-center items-center`}
         >
         {dataNavBar.map((link, index) => (
           <li key={link.id} className="p-4 hover:text-hover-green" onClick={toggleMenu}>
